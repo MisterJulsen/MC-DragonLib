21d00e6b1fb87d5d0f1a3ecabe3b789fd449f66b
MisterJulsen <julian.ramsperger@t-online.de>
2023-12-05 20:20:29 +0100

    New Constructor

--- src/main/java/de/mrjulsen/mcdragonlib/common/Location.java
+++ src/main/java/de/mrjulsen/mcdragonlib/common/Location.java
@@ -2,6 +2,7 @@ package de.mrjulsen.mcdragonlib.common;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 public class Location {
@@ -32,6 +33,14 @@ public class Location {
         this.z = z;
         this.dimension = dimension;
         this.generateBlockPos();
+    }
+
+    public Location(BlockPos pos, Level level) {
+        this.x = pos.getX();
+        this.y = pos.getY();
+        this.z = pos.getZ();
+        this.dimension = level.dimension().location().toString();
+        this.blockPos = pos;
     }
 
     public BlockPos getLocationBlockPos() {
