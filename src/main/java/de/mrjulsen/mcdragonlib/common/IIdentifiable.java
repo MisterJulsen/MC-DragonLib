54a7bfd203219eeb6fd4a1f5440514a0e4377c3a
MisterJulsen <julian.ramsperger@t-online.de>
2023-12-05 20:20:18 +0100

    Changes UUID to String

--- src/main/java/de/mrjulsen/mcdragonlib/common/IIdentifiable.java
+++ src/main/java/de/mrjulsen/mcdragonlib/common/IIdentifiable.java
@@ -1,11 +1,5 @@
 package de.mrjulsen.mcdragonlib.common;
 
-import java.util.UUID;
-
 public interface IIdentifiable {
-    UUID getId();
-    
-    default UUID generateId() {
-        return UUID.randomUUID();
-    }
+    String getId();
 }
