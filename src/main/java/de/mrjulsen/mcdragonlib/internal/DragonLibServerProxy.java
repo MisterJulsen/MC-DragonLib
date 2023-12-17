48a9266804668627600f4608cca95ea02ebb38d2
MisterJulsen <geisterdrache02@gmail.com>
2023-12-17 15:01:48 +0100

    Renamed internal server proxy class

---
+++ src/main/java/de/mrjulsen/mcdragonlib/internal/DragonLibServerProxy.java
@@ -0,0 +1,12 @@
+package de.mrjulsen.mcdragonlib.internal;
+
+import de.mrjulsen.mcdragonlib.setup.IProxy;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+public class DragonLibServerProxy implements IProxy {
+
+    @Override
+    public void setup(FMLCommonSetupEvent event) {
+    }
+
+}
